9d7810cb0477f61282817e2f4784f03ff65bb818
Polina <vslpoulin@gmail.com>
2018-02-05 15:45:49 +0300

    Fixed Task3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int a = 1; // Начальное значение
-        int b = 21; // Конечное значение
-        int h = 4; // Шаг
+
+        Scanner sc = new Scanner(System.in);
+        int a, b, h;
+
+        do {
+            System.out.println("Enter initial number of the segment:");
+            a = sc.nextInt();
+
+            System.out.println("Enter final number of the segment:");
+            b = sc.nextInt();
+
+            System.out.println("Enter step value:");
+            h = sc.nextInt();
+
+            if (checkParameters(a, b, h)) {
+                System.out.println("Incorrect! Enter parameters again!");
+                System.out.println("--------------------------------------");
+            }
+
+        } while (checkParameters(a, b, h));
+
 
         System.out.println("Argument | Function");
         System.out.println("--------------------");
 
-        for (int i = a; i < b; i += h) {
-            double f = (Math.tan(2 * i)) - 3;
+        for (int i = a; i <= b; i += h) {
+            double f = functionCounter(i);
             System.out.println(String.format("%-8d | %f", i, f));
         }
+    }
+
+    private static double functionCounter(int i) {
+        return (Math.tan(2 * i)) - 3;
+    }
+
+    private static boolean checkParameters(int a, int b, int h) {
+        return ((a > b) || ((b - a) < h) || (a == 0 && b == 0 && h == 0));
     }
 }
