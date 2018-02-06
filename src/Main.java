614c7e99700ab709d4eba3967ddc955ce3d71232
Polina <vslpoulin@gmail.com>
2018-02-06 20:49:24 +0300

    Fixed Test3

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,17 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int a, b, h;
+        double a, b, h;
 
         do {
             System.out.println("Enter initial number of the segment:");
-            a = sc.nextInt();
+            a = sc.nextDouble();
 
             System.out.println("Enter final number of the segment:");
-            b = sc.nextInt();
+            b = sc.nextDouble();
 
             System.out.println("Enter step value:");
-            h = sc.nextInt();
+            h = sc.nextDouble();
 
             if (checkParameters(a, b, h)) {
                 System.out.println("Incorrect! Enter parameters again!");
@@ -25,20 +25,20 @@ public class Main {
         } while (checkParameters(a, b, h));
 
 
-        System.out.println("Argument | Function");
+        System.out.println("Argument  |  Function");
         System.out.println("--------------------");
 
-        for (int i = a; i <= b; i += h) {
+        for (double i = a; i <= b; i += h) {
             double f = functionCounter(i);
-            System.out.println(String.format("%-8d | %f", i, f));
+            System.out.println(String.format("%-8f  |  %f", i, f));
         }
     }
 
-    private static double functionCounter(int i) {
+    private static double functionCounter(double i) {
         return (Math.tan(2 * i)) - 3;
     }
 
-    private static boolean checkParameters(int a, int b, int h) {
-        return ((a > b) || ((b - a) < h) || (a == 0 && b == 0 && h == 0));
+    private static boolean checkParameters(double a, double b, double h) {
+        return ((a > b) || ((b - a) < h) || (a == 0 && b == 0 && h == 0) || (h < 0));
     }
 }
